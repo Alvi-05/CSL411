b8106e60b900c5a96e772a6064f1f1a52d268430
Alvi-05 <alviya945@gmail.com>
2025-07-16 00:53:20 +0530

    2 exp

---
+++ c1/enfa.c
@@ -0,0 +1,114 @@
+#include<stdio.h>
+#include<string.h>
+char result[20][20], copy[3], states[20][20];
+
+void add_state(char a[3], int i) {
+
+  strcpy(result[i], a);
+
+}
+
+void display(int n) {
+
+  int k = 0;
+
+  printf("\n  Epsilon closure of %s = { ", copy);
+
+  while (k < n) {
+
+    printf(" %s", result[k]);
+
+    k++;
+
+  }
+
+  printf(" }\n");
+
+}
+
+int main() {
+
+  FILE * INPUT;
+
+  INPUT = fopen("automata.txt", "r");
+
+  char state[3];
+
+  int end, i = 0, n, k = 0;
+
+  char state1[3], input[3], state2[3];
+
+  printf("\n Enter the no of states: ");
+
+  scanf("%d",&n);
+
+  printf("\n Enter the states max =20 \n");
+
+  for (k = 0; k < n; k++) {
+
+    scanf("%s", states[k]);
+
+  }
+
+  for (k = 0; k < n; k++) {
+
+    i = 0;
+
+    strcpy(state, states[k]);
+
+    strcpy(copy, state);
+
+    add_state(state, i++);
+
+    while (1) {
+
+      end = fscanf(INPUT, "%s%s%s", state1, input, state2);
+
+      if (end == EOF) {
+
+        break;
+
+      }
+
+      if (strcmp(state, state1) == 0) {
+
+        if (strcmp(input, "e") == 0) {
+
+          add_state(state2, i++);
+
+          strcpy(state, state2);
+
+        }
+
+      }
+
+    }
+
+    display(i);
+
+    rewind(INPUT);
+
+  }
+
+  return 0;
+
+}
+/*
+
+$ gcc e-closure.c
+$ ./a.out
+
+Enter the no of states(max 20): 3
+
+ Enter the states
+q0
+q1
+q2
+
+  Epsilon closure of q0 = {  q0 q1 q2 }
+
+  Epsilon closure of q1 = {  q1 q2 }
+
+  Epsilon closure of q2 = {  q2 }
+
+  */
