fe47ffbd8ce3692810eb7682d1b9fce614cb555a
Alvi-05 <alviya945@gmail.com>
2025-07-16 23:28:11 +0530

    fixed dfa

--- c1/dfa.c
+++ c1/dfa.c
@@ -6,8 +6,23 @@
 
 char nfa_states[MAX][MAX], dfa_states[100][MAX];
 char symbols[MAX];
-int nfa_transitions[MAX][MAX][MAX], dfa_transitions[100][MAX];
+int nfa_transitions[MAX][MAX][MAX]; // Stores 1 if transition exists, 0 otherwise
+int dfa_transitions[100][MAX];      // Stores index of next DFA state
 int nfa_state_count, symbol_count, dfa_state_count = 0;
+
+// Function to sort a string alphabetically
+void sort_string(char *str) {
+    int len = strlen(str);
+    for (int i = 0; i < len - 1; i++) {
+        for (int j = i + 1; j < len; j++) {
+            if (str[i] > str[j]) {
+                char temp = str[i];
+                str[i] = str[j];
+                str[j] = temp;
+            }
+        }
+    }
+}
 
 // Check if a state set already exists in DFA
 int state_exists(char state[]) {
@@ -18,13 +33,13 @@ int state_exists(char state[]) {
     return -1;
 }
 
-// Get state index
+// Get state index from NFA states
 int get_index(char state[]) {
     for (int i = 0; i < nfa_state_count; i++) {
         if (strcmp(nfa_states[i], state) == 0)
             return i;
     }
-    return -1;
+    return -1; // Should not happen with valid input
 }
 
 // Add DFA state
@@ -34,49 +49,64 @@ void add_dfa_state(char state[]) {
 
 // Generate transitions for DFA
 void generate_dfa() {
-    char temp[MAX];
+    char initial_dfa_state[MAX] = "";
+    // Assuming the initial NFA state is a single character state
+    strcat(initial_dfa_state, nfa_states[0]); 
+    add_dfa_state(initial_dfa_state);
+
     int front = 0;
-
-    add_dfa_state(nfa_states[0]);  // Start from initial NFA state
-
     while (front < dfa_state_count) {
-        char current[MAX];
-        strcpy(current, dfa_states[front]);
+        char current_dfa_state[MAX];
+        strcpy(current_dfa_state, dfa_states[front]);
 
         for (int s = 0; s < symbol_count; s++) {
-            char new_state[MAX] = "";
+            char new_dfa_state_temp[MAX] = ""; // Temporary buffer for new state
+            char combined_next_states[MAX] = ""; // To collect all next states from current DFA state
 
-            for (int i = 0; i < strlen(current); i++) {
-                int index = get_index((char[]){current[i], '\0'});
+            // Iterate through each NFA state in the current DFA state
+            for (int i = 0; i < strlen(current_dfa_state); i++) {
+                char nfa_char[2];
+                nfa_char[0] = current_dfa_state[i];
+                nfa_char[1] = '\0';
+                int nfa_index = get_index(nfa_char);
 
+                // Find all NFA states reachable from nfa_char on symbol 's'
                 for (int j = 0; j < nfa_state_count; j++) {
-                    if (nfa_transitions[index][s][j]) {
-                        char ch = 'A' + j;
-                        if (!strchr(new_state, ch)) {
-                            int len = strlen(new_state);
-                            new_state[len] = ch;
-                            new_state[len + 1] = '\0';
+                    if (nfa_transitions[nfa_index][s][j]) {
+                        char next_nfa_char = nfa_states[j][0]; // Assuming single character NFA states
+                        if (!strchr(combined_next_states, next_nfa_char)) {
+                            int len = strlen(combined_next_states);
+                            combined_next_states[len] = next_nfa_char;
+                            combined_next_states[len + 1] = '\0';
                         }
                     }
                 }
             }
 
-            int existing = state_exists(new_state);
-            if (existing == -1 && strlen(new_state) > 0)
-                add_dfa_state(new_state);
+            sort_string(combined_next_states); // Sort the collected states
 
-            dfa_transitions[front][s] = state_exists(new_state);
+            int existing_dfa_state_index = -1;
+            if (strlen(combined_next_states) > 0) {
+                existing_dfa_state_index = state_exists(combined_next_states);
+                if (existing_dfa_state_index == -1) {
+                    add_dfa_state(combined_next_states);
+                    existing_dfa_state_index = dfa_state_count - 1; // Index of newly added state
+                }
+            }
+            
+            dfa_transitions[front][s] = existing_dfa_state_index;
         }
         front++;
     }
 }
+
 
 int main() {
     int i, j, k;
 
     printf("Enter number of NFA states: ");
     scanf("%d", &nfa_state_count);
-    printf("Enter NFA state names (single characters):\n");
+    printf("Enter NFA state names (single characters, e.g., A B C):\n");
     for (i = 0; i < nfa_state_count; i++)
         scanf("%s", nfa_states[i]);
 
@@ -86,19 +116,40 @@ int main() {
     for (i = 0; i < symbol_count; i++)
         scanf(" %c", &symbols[i]);
 
+    // Initialize nfa_transitions to 0
+    for (i = 0; i < MAX; i++) {
+        for (j = 0; j < MAX; j++) {
+            for (k = 0; k < MAX; k++) {
+                nfa_transitions[i][j][k] = 0;
+            }
+        }
+    }
+
     printf("Enter transitions in format: from_state symbol to_state\n");
     printf("Enter -1 to stop\n");
 
     while (1) {
-        char from[MAX], sym, to[MAX];
+        char from[MAX], sym_char_str[2], to[MAX];
         scanf("%s", from);
         if (strcmp(from, "-1") == 0) break;
-        scanf(" %c %s", &sym, to);
+        scanf(" %s %s", sym_char_str, to); // Read symbol as string to handle 'a', 'b', etc.
+        char sym = sym_char_str[0]; // Take the first character as the symbol
+
         int from_i = get_index(from);
         int to_i = get_index(to);
-        int sym_i = strchr(symbols, sym) - symbols;
+        int sym_i = -1;
+        for (int x = 0; x < symbol_count; x++) {
+            if (symbols[x] == sym) {
+                sym_i = x;
+                break;
+            }
+        }
 
-        nfa_transitions[from_i][sym_i][to_i] = 1;
+        if (from_i != -1 && to_i != -1 && sym_i != -1) {
+            nfa_transitions[from_i][sym_i][to_i] = 1;
+        } else {
+            printf("Invalid transition input. Please check state names and symbols.\n");
+        }
     }
 
     generate_dfa();
@@ -115,7 +166,7 @@ int main() {
             if (dfa_transitions[i][j] != -1)
                 printf("%s\t", dfa_states[dfa_transitions[i][j]]);
             else
-                printf("-\t");
+                printf("{}\t"); // Represent empty transition
         }
         printf("\n");
     }
